0e13779bc034a420755f6c13c5265fc65a6aa14f
onyx <onyxl09@uia.no>
2020-11-06 09:18:24 +0100

    test server

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 const express = require(`express`);
-const app = express ();
-app.get(`/`, (reg, res) =>{
-    res.send("onyx");
+
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+class Task{
+    constructor(id, title, subject, task_text){
+        this.id = id;
+        this.title = title;
+        this.subject = subject;
+        this.task_text = task_text;
+    }
+}
+
+Tasks = []
+// Task data
+
+Tasks.push(new Task(1, ' lese til eksamen', 'eksamen', 'les fra kap 12 til kap 25 .'))
+Tasks.push(new Task(2, 'se på tv ', 'Gjøremål fotball kamp', 'start mot brann i kveld .'))
+Tasks.push(new Task(3, 'klasse tur', 'Paris', 'vi skal ut og spise .'))
+
+// Rounters for tasks - will return all tasks
+app.get(`/task`, (reg, res) =>{
+    res.send(Tasks);
 });
 
-app.get(`/name`, (reg, res) =>{
-    res.send("my name is oooo");
+// Will return one task
+app.get(`/task/:id`, (reg, res) =>{
+    // Id som kommer fra url
+    const id = reg.params.id;
+    if(id > Tasks.length){
+        return res.send('Bad request', 400);
+    }
+    // Vi ser om vi har en task med id som kommer fra url
+    for(let i = 0; i < Tasks.length; i++){
+        if(Tasks[i].id == id){
+            return  res.send(Tasks[i]);
+        }
+    }
+
 });
 
-const PORT = process.env.PORT|| 8080;
+// Route to create new task
+app.post(`/task`, (req, res) => {
+    console.log(req);
+    console.log(req.body)
+    Tasks.push(req.body)
+    return res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,() => console.log(`running on ${PORT}`));
 
