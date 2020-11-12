695755dc00f50620680b14bd57cd49268d04a1da
smedsrun <of.smedsrud@gmx.com>
2020-11-12 13:47:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,11 +53,6 @@ server.post(`/task`, (req, res) => {
     return res.sendStatus(200);
 });
 
-
-
-
-});
-
 const PORT = process.env.PORT || 8080;
 server.listen(PORT,() => console.log(`running on ${PORT}`));
 
