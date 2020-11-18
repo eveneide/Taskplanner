e57d6e5380d0902bc7e7ab265e572f1439025a40
smedsrun <of.smedsrud@gmx.com>
2020-11-18 11:04:42 +0100

    kryptering basert på Caesar, la til .gitignore fil i public

---
+++ modules/taskplannercipher.js
@@ -0,0 +1,36 @@
+
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz 0123456789.,:;+-?ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+function encrypt (message, offset){
+    const cipher = createCipher(alphabet, offset);
+    const message = cipherText.split("").reduce((c, key) => {
+        c += alphabet[cipher.indexOf(key)]
+        return c;
+    })
+    return cipherText;
+}
+
+function decrypt (cipherText, offset) {
+    const cipher = createCipher(alphabet, offset);
+    const message = cipherText.split("").reduce((c, key) => {
+        c += alphabet[cipher.indexOf(key)]
+        return c;
+    })
+    return message;
+
+}
+
+function createCipher(alphabet, count){
+    let cipher = [...alphabet];
+    count -= cipher.length * Math.floor(count / cipher.length);
+    cipher.push.apply(cipher, cipher.splice(0, count));
+    return cipher;
+}
+
+module.exports = {
+
+    encrypt,
+    decrypt
+
+};
