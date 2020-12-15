0f6749471541cd1de777dd5127175205bf5e2550
smedsrun <of.smedsrud@gmx.com>
2020-12-15 01:06:32 +0100

    Revert "deployment fix"
    This reverts commit 5376910cf5385222d7416563679c1b2eb10da396.

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ server.put('/task', async (req, res) => {
 
 //Start server
 
-const PORT = process.env.port || 8080;
+const port = process.env.port || 8080;
 
-server.listen(PORT, () => {
-  console.log(` server has started on port ${PORT}`)
+server.listen(port, () => {
+  console.log(` server has started on port ${port}`)
 });
